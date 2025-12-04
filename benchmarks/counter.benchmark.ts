3b1d32f7c4cc939ca32b7e868f4467d39e543386
Paperclip Minimizer <minim@defi.sucks>
2025-12-04 18:39:09 -0300

    feat: shitton of benchmarks

--- benchmarks/counter.benchmark.ts
+++ benchmarks/counter.benchmark.ts
@@ -56,11 +56,51 @@ export default class CounterContractBenchmark extends Benchmark {
     const methods: ContractFunctionInteractionCallIntent[] = [
       {
         caller: deployer,
-        action: counterContract.withWallet(wallet).methods.log_sequential_32_8(),
+        action: counterContract.withWallet(wallet).methods.log_sequential_16_2()
       },
       {
         caller: deployer,
-        action: counterContract.withWallet(wallet).methods.log_sequential_extra_work_32_8(),
+        action: counterContract.withWallet(wallet).methods.log_sequential_16_4()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_16_8()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_16_16()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_32_1()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_32_2()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_32_4()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_32_8()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_64_1()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_64_2()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_64_4()
+      },
+      {
+        caller: deployer,
+        action: counterContract.withWallet(wallet).methods.log_sequential_extra_work_32_8()
       },
     ];
 
